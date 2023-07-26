b398416e5708f7960d892521beaa6747dea0f7c6
David Nelson <larva.code15@gmail.com>
2023-07-26 17:15:47 +0100

    Pushing particles design ⚛️

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,16 +1,33 @@
+import { useCallback } from "react";
 import Navbar from "../components/Navbar";
 import Header from "./Header";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 
 interface PublicProps {
     children: any;
     pageTitle: string;
 }
 const PublicLayout: React.FC<PublicProps> = ({ children, pageTitle }) => {
+    const particlesInit = useCallback(async (engine: any) => {
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async () => {
+        //console.log(container);
+    }, []);
+
     return (
         <div>
             <Header title={pageTitle} />
             <Navbar />
             <div className="App" id="butter">
+                <Particles
+                    id="tsparticles"
+                    url="https://res.cloudinary.com/ruthless-labs/raw/upload/v1690388068/spacebank/wextkw3tmvwzwhs0jsmx.json"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                />
                 {children}
             </div>
         </div>
